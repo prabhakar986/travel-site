10bf7d456ef6a5312f208f5f22aeeef9511564e7
prabhakar <prabhakar.webwork@gmail.com>
2020-12-14 19:07:03 +0530

    webpack dev server set-up is complete for CSS/js, html files and to open the app at any device using IPv4 addreess with the port number set in webconfig file

--- webpack.config.js
+++ webpack.config.js
@@ -8,15 +8,31 @@ const postCSSPlugins = [
 ]
 
 module.exports = {
-    mode: "development",
-
+    
     entry: './app/assets/scripts/App.js',
     output: {
         path:path.resolve(__dirname, "app"),
         filename: "bundled.js"
     },
-    watch: true,
 
+    devServer: {
+        /*to auto reload all html files in app folder*/
+        before: function(app, server) {
+            server._watch( './app/**/*.html' )
+        },
+
+        /*to auto reload all CSS and js files in app folder*/
+        contentBase: path.join(__dirname, 'app'),
+
+        hot: true,
+        port: 3000,
+
+        /*to open in any device using ipv4 address(192.168.1.6) with port number (:3000) as 192.168.1.6:3000*/
+        host: '0.0.0.0'
+    },
+
+    mode: "development",
+    
     module: {
         rules: [
             {
@@ -24,6 +40,6 @@ module.exports = {
                 use: ['style-loader', 'css-loader?url=false', {loader: "postcss-loader", options: {postcssOptions:{plugins: postCSSPlugins}}}]
             }
         ]
-    }
+    }    
 
 }
